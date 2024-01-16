45c8295ff6e66dab13e136e7ef6fe067842fcd8a
Alex <alex95.taylor@gmail.com>
2024-01-15 20:23:10 -0500

    Create ProductList component

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header";
+
+function ProductList () {
+    return (
+        <>
+            <Header />
+            <h1>Shop</h1>
+        </>
+    )
+}
+
+export default ProductList;
