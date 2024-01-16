d244782debb38c49a41dd573e54489651afce90b
Alex <alex95.taylor@gmail.com>
2024-01-15 20:23:46 -0500

    Create Homepage component

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header";
+
+function Homepage() {
+    return (
+        <>
+            <Header />
+            <h1>Welcome to Electronic Apparels!</h1>
+        </>
+    )
+}
+
+export default Homepage;
