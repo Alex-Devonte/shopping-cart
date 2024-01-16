b0877896e7f77b59167fc0b63c7ff274a0815a1a
Alex <alex95.taylor@gmail.com>
2024-01-15 20:23:10 -0500

    Create Cart component

---
+++ src/components/Cart.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+
+function Cart () {
+    return (
+        <>
+            <Header />
+            <h1>Cart</h1>
+        </>
+        
+    )
+}
+
+export default Cart;
