9c0e608dbea9a02b3e212a45427b011c8ec82f6c
Alex <alex95.taylor@gmail.com>
2024-01-18 05:23:12 -0500

    Add logic for displaying cart info

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,12 +1,65 @@
+import { useContext } from "react";
 import Header from "./Header";
+import { CartContext } from "./CartProvider";
 
 function Cart () {
+    const { cart, getTotal, updateCart } = useContext(CartContext);
+
+    const minQty = 1;
+    const maxQty = 99;
+
+    const handleQtyChange = (item, event) => {
+        let newQty = parseInt(event.target.value);
+
+        //Prevent qty from being set outside the bounds
+        if (!isNaN(newQty)) {
+            if (newQty > maxQty) {
+                newQty = maxQty;
+            } else if (newQty < minQty) {
+                newQty = minQty;
+            }
+            updateCart(item.cartItem, newQty);
+        } else {
+            //Prevent NaN
+            updateCart(item.cartItem, '');
+        }
+    }
+
     return (
         <>
             <Header />
-            <h1>Cart</h1>
+            <div>
+                {cart.length > 0 ? (
+                    <>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <td>Item</td>
+                                    <td>Price</td>
+                                    <td>Quantity</td>
+                                    <td>Total</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {cart.map((item) => (
+                                    <tr key={item.cartItem.id}>
+                                        <td>img</td>
+                                        <td>{item.cartItem.price}</td>
+                                        <td><input type="number" name="quantity" min={minQty} max={maxQty} value={item.qty}  onChange={(e) => handleQtyChange(item, e)}/></td>
+                                        <td>{(item.cartItem.price * item.qty).toFixed(2)}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                        <div>
+                            <p>Total: {getTotal()}</p>
+                        </div>
+                    </>
+                ) : (
+                    <p>Your cart is empty</p>
+                )}
+            </div>
         </>
-        
     )
 }
 
