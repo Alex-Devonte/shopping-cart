1dd9d2c2f14a43408e8cf8e506079e47f16eca7d
Alex <alex95.taylor@gmail.com>
2024-01-18 15:40:01 -0500

    Add Header component to ProductDetail page

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,20 +1,24 @@
 import { useLocation } from "react-router-dom";
+import Header from "./Header";
 
 function ProductDetail() {
     const {state} = useLocation();
     const product = state.productData;
 
     return (
-        <div>
+        <>
+            <Header />
             <div>
-                <img src={product.image} />
+                <div>
+                    <img src={product.image} />
+                </div>
+                <div>   
+                    <p>{product.title}</p>
+                    <p>{product.description}</p>
+                    <p>{product.price}</p>
+                </div>
             </div>
-            <div>   
-                <p>{product.title}</p>
-                <p>{product.description}</p>
-                <p>{product.price}</p>
-            </div>
-        </div>
+        </>
     )
 }
 
