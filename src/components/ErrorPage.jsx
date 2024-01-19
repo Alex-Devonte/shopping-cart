304dcfd438beda2fda958edb5cf08c3da7b2f3af
Alex <alex95.taylor@gmail.com>
2024-01-18 21:44:34 -0500

    Create component for bad routes

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function ErrorPage() {
+    return (
+        <div>
+            <h1>This route does not exist!</h1>
+            <Link to="/">Click here to return to the homepage.</Link>
+        </div>
+    )
+}
+
+export default ErrorPage;
