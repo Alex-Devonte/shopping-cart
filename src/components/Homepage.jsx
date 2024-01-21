9b8c424f1fae73d696a5dbaeb07fc4f3ff6f04a2
Alex <alex95.taylor@gmail.com>
2024-01-20 23:55:05 -0500

    Add shop now button

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,13 @@
+import { useNavigate } from "react-router-dom";
 import Header from "./Header";
 
 function Homepage() {
+    const navigate = useNavigate();
     return (
         <>
             <Header />
             <h1>Welcome to Electronic Apparels!</h1>
+            <button type="button" onClick={() => navigate("/shop")}>Shop Now</button>
         </>
     )
 }
