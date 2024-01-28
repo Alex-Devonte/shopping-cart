d0cf17ab6e6a4e7a89130c3f47d327de5beecb9f
Alex <alex95.taylor@gmail.com>
2024-01-28 15:40:41 -0500

    Add styles for components

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function ErrorPage() {
     return (
-        <div>
+        <div id="error">
             <h1>This route does not exist!</h1>
             <Link to="/">Click here to return to the homepage.</Link>
         </div>
